f34a7c46f6b569be9d4afebbd979d8b25a60a2f1
Tasfiha Saba <77076887+tasfihasaba@users.noreply.github.com>
2023-05-23 23:50:44 -0400

    Update & Delete link in the db

--- PERN-API/server/queries.js
+++ PERN-API/server/queries.js
@@ -21,18 +21,66 @@ const getLinks = (req, res) => {
   });
 };
 
-const getAllData = (req, res) => {
-  pool.query('SELECT * FROM links', (error, result) => {
+const createLink = (req, res) => {
+  const { title, url } = req.body; // Assuming title and url are provided in the request body
+
+  if (!title || !url) {
+    res.status(400).json({ error: 'Title and URL are required' });
+    return;
+  }
+
+  pool.query(
+    'INSERT INTO links (title, url) VALUES ($1, $2)',
+    [title, url],
+    (error) => {
+      if (error) {
+        res.status(500).json({ error: 'Error creating link' });
+      } else {
+        res.status(201).json({ message: 'Link created successfully' });
+      }
+    }
+  );
+};
+
+const updateLink = (req, res) => {
+  const { id, title, url } = req.body; // Assuming id, title, and url are provided in the request body
+
+  if (!id || !title || !url) {
+    res.status(400).json({ error: 'ID, title, and URL are required' });
+    return;
+  }
+
+  pool.query(
+    'UPDATE links SET title = $1, url = $2 WHERE id = $3',
+    [title, url, id],
+    (error) => {
+      if (error) {
+        res.status(500).json({ error: 'Error updating link' });
+      } else {
+        res.status(200).json({ message: 'Link updated successfully' });
+      }
+    }
+  );
+};
+
+const deleteLink = (req, res) => {
+  const id = req.params.id; // Assuming the link ID is passed as a route parameter
+
+  pool.query('DELETE FROM links WHERE id = $1', [id], (error) => {
     if (error) {
-      res.status(500).json({ error: 'Error retrieving data' });
+      res.status(500).json({ error: 'Error deleting link' });
     } else {
-      res.status(200).json(result.rows);
+      res.status(200).json({ message: 'Link deleted successfully' });
     }
   });
 };
 
+app.use(express.json());
+
 app.get('/links', getLinks);
-app.get('/alldata', getAllData);
+app.post('/links', createLink);
+app.put('/links', updateLink);
+app.delete('/links/:id', deleteLink);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
