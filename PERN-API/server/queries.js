79b45901c68aca23a45b00ba23735a3a1d730c15
Tasfiha Saba <77076887+tasfihasaba@users.noreply.github.com>
2023-05-23 23:45:51 -0400

    Add new link in the db

--- PERN-API/server/queries.js
+++ PERN-API/server/queries.js
@@ -3,7 +3,6 @@ const { Pool } = require('pg');
 
 const app = express();
 
-
 const pool = new Pool({
   user: 'me',
   host: 'localhost',
@@ -11,7 +10,6 @@ const pool = new Pool({
   password: 'password',
   port: 5432,
 });
-
 
 const getLinks = (req, res) => {
   pool.query('SELECT * FROM links ORDER BY id ASC', (error, result) => {
@@ -23,8 +21,31 @@ const getLinks = (req, res) => {
   });
 };
 
-app.get('/links', getLinks);
+const createLink = (req, res) => {
+  const { title, url } = req.body; // Assuming title and url are provided in the request body
 
+  if (!title || !url) {
+    res.status(400).json({ error: 'Title and URL are required' });
+    return;
+  }
+
+  pool.query(
+    'INSERT INTO links (title, url) VALUES ($1, $2)',
+    [title, url],
+    (error) => {
+      if (error) {
+        res.status(500).json({ error: 'Error creating link' });
+      } else {
+        res.status(201).json({ message: 'Link created successfully' });
+      }
+    }
+  );
+};
+
+app.use(express.json());
+
+app.get('/links', getLinks);
+app.post('/links', createLink);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
